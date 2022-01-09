305578d3c37f32f1d442bc2f67ea244a63508be4
wnortier <warwick.nortier@gmail.com>
2022-01-09 15:07:10 +0200

    added cart data file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,10 @@
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main className="block col-2">
+      <h2>Products</h2>
+      <div className="row"></div>
+    </main>
+  );
 };
 
 export default Main;
